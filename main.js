88fe1be7e8e3d84c17163b61a179d9bbee075e74
Vogelbecker <info@vogelbecker.de>
2020-06-25 20:10:15 +0200

    Update main.js

--- main.js
+++ main.js
@@ -9,7 +9,8 @@
 // The adapter-core module gives you access to the core ioBroker functions
 // you need to create an adapter
 const utils = require('@iobroker/adapter-core');
-const retus = require("retus");
+const request = require('request'); 
+const retus = require('retus');
 
 // Load your modules here, e.g.:
 // const fs = require("fs");
